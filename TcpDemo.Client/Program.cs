b9ef4ef320afff634aa2dbc3468b0dc6afa4a997
Starinin Andrey <starininandrey@gmail.com>
2024-10-20 13:48:48 +0300

    Initial commit

---
+++ TcpDemo.Client/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+using var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+try
+{
+    await socket.ConnectAsync("127.0.0.1", 8888);
+    Console.WriteLine($"Подключение к {socket.RemoteEndPoint} установлено");
+    
+    await using var stream = new NetworkStream(socket);
+    var message = "Hello World!";
+    var data = Encoding.UTF8.GetBytes(message);
+    await stream.WriteAsync(data);
+    
+    await stream.FlushAsync();
+    Console.WriteLine($"Сообщение \"{message}\" отправлено");
+    
+    var buffer = new byte[1024];
+    var bytesRead = await stream.ReadAsync(buffer);
+    var receivedMessage = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+    Console.WriteLine($"Получено сообщение \"{receivedMessage}\"");
+}
+catch (SocketException)
+{
+    Console.WriteLine($"Не удалось установить подключение с {socket.RemoteEndPoint}");
+}
---
+++ TcpDemo.Server/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var ipPoint = new IPEndPoint(IPAddress.Any, 8888);
+using var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+socket.Bind(ipPoint);
+socket.Listen();
+Console.WriteLine("Сервер запущен. Ожидание подключений...");
+
+using var client = await socket.AcceptAsync();
+Console.WriteLine($"Адрес подключенного клиента: {client.RemoteEndPoint}");
+
+await using var stream = new NetworkStream(client);
+var byteArray = new byte[1024];
+var bytes = await stream.ReadAsync(byteArray);
+var response = Encoding.UTF8.GetString(byteArray, 0, bytes);
+Console.WriteLine(response);
+
+var message = Encoding.UTF8.GetBytes($"Ваше сообщение: {response} получено!");
+await stream.WriteAsync(message);
